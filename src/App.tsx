67aebc2ef12b5e57e28e725888e638c075b92c8e
Dollars <aomsup5185@gmail.com>
2024-06-21 17:05:23 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import '../src/components/Nav/Nav.css';
+import './App.css'
+import Nav from './components/Nav/Nav'
+
+
+function App() {
+
+  return (
+    <>
+    <Nav/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Nav/Nav.tsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import './Nav.css'
+import { MdOutlinePhoneInTalk } from "react-icons/md";
+import { IoMoonOutline } from "react-icons/io5";
+import { LuSun } from "react-icons/lu";
+
+function Nav() {
+
+    const [isChecked, setIsChecked] = useState<boolean>(false);
+
+    const handleToggleChange = () => {
+        setIsChecked(!isChecked);
+        document.body.style.backgroundColor = isChecked ? '#fff' : '#191c20';
+        
+    };
+
+    return (
+        <nav>
+            <div className='nav-container'>
+                <div className='nav-logo'>
+                    <span>JoJo</span>
+                    <span>Studio</span>
+                </div>
+                <div className='nav-menu'>
+                    <div className='nav-menu-list'>
+                        <input type="checkbox" id="darkmode-toggle" checked={isChecked} onChange={handleToggleChange} />
+                        <label htmlFor="darkmode-toggle">
+                            <div style={{ color: isChecked ? 'black' : '#31B7E9' }}>
+                                <LuSun className='sun' />
+                            </div>
+                            <div style={{ color: isChecked ? '#31B7E9' : 'black' }}>
+                                <IoMoonOutline className='moon' />
+                            </div>
+                        </label>
+                    </div>
+                    <div className='nav-menu-btn'>
+                        <MdOutlinePhoneInTalk className="phone" />
+                        <h4>ปรึกษาฟรี!</h4>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Nav
