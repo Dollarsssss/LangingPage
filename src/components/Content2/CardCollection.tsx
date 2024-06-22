6c903c477d03c76dae497941f9f363bd24a435c2
Dollars <aomsup5185@gmail.com>
2024-06-22 21:29:58 +0700

    Swiper Card

--- src/components/Content2/CardCollection.tsx
+++ src/components/Content2/CardCollection.tsx
@@ -1,24 +1,35 @@
 import './CardCollection.css'
 import ServiceCard from './ServiceCard/ServiceCard';
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+
 function CardCollection() {
 
   const services = [
     {
-      title: <>บริการออกแบบเว็บไซต์<br/>Web Design</>,
+      title: <>บริการออกแบบเว็บไซต์<br />Web Design</>,
       subtitle: <>รับออกแบบเว็บไซต์ให้สวย พร้อมใช้งานเหมาะสมกับธุรกิจคุณ</>
     },
     {
-      title: <>รับทำเว็บไซต์<br/>Web Development</>,
+      title: <>รับทำเว็บไซต์<br />Web Development</>,
       subtitle: <>รับพัฒนาเว็บไซต์ธุรกิจ ร้านค้าออนไลน์ ฟังก์ชั่นครบทุกการใช้งาน</>
     },
     {
-      title: <>รับทำแอปพลิเคชั่น<br/>Mobile Development</>,
-      subtitle:<>รับพัฒนาแอปพลิเคชั่น รองรับระบบ Android และ IOS</>
+      title: <>รับทำแอปพลิเคชั่น<br />Mobile Development</>,
+      subtitle: <>รับพัฒนาแอปพลิเคชั่น รองรับระบบ Android และ IOS</>
     },
     {
-      title: <>บริการ Custom LINE API <br/> LINE DEV</>,
+      title: <>บริการ Custom LINE API <br /> LINE DEV</>,
       subtitle: <>ทางออกในการเชื่อมต่อธุรกิจของคุณเข้ากับ LINE OA พิเศษเฉพาะธุรกิจคุณ</>
+    },
+    {
+      title: <>รับทำ SEO<br />Search Engine Optimization</>,
+      subtitle: <>บริการ SEO Suggestion ให้ เว็บไซต์ของคุณ มีโอกาสติดอันดับบน google</>
     }
   ];
 
@@ -28,23 +39,52 @@ function CardCollection() {
         <div className='card-title'>
           <h1>บริการของเรา</h1>
         </div>
-        <div className='grid'>
-          <ServiceCard
-            title={services[0].title}
-            subtitle={services[0].subtitle}
-          />
-          <ServiceCard
-            title={services[1].title}
-            subtitle={services[0].subtitle}
-          />
-          <ServiceCard
-            title={services[2].title}
-            subtitle={services[0].subtitle}
-          />
-          <ServiceCard
-            title={services[3].title}
-            subtitle={services[0].subtitle}
-          />
+        <div >
+            <Swiper
+              slidesPerView={1}
+              spaceBetween={10}
+              pagination={{
+                clickable: true,
+              }}
+              breakpoints={{
+                640: {
+                  slidesPerView: 1,
+                  spaceBetween: 20,
+                },
+                768: {
+                  slidesPerView: 3,
+                  spaceBetween: 40,
+                },
+                1024: {
+                  slidesPerView: 4,
+                  spaceBetween: 20,
+                },
+              }}
+
+              className="mySwiper"
+            >
+              <SwiperSlide><ServiceCard
+                title={services[0].title}
+                subtitle={services[0].subtitle}
+              /></SwiperSlide>
+              <SwiperSlide><ServiceCard
+                title={services[1].title}
+                subtitle={services[1].subtitle}
+              /></SwiperSlide>
+              <SwiperSlide><ServiceCard
+                title={services[2].title}
+                subtitle={services[2].subtitle}
+              /></SwiperSlide>
+              <SwiperSlide><ServiceCard
+                title={services[3].title}
+                subtitle={services[3].subtitle}
+              /></SwiperSlide>
+              <SwiperSlide> <SwiperSlide><ServiceCard
+                title={services[4].title}
+                subtitle={services[4].subtitle}
+              /></SwiperSlide></SwiperSlide>
+            </Swiper>
+      
         </div>
       </div>
     </section>
@@ -52,3 +92,21 @@ function CardCollection() {
 }
 
 export default CardCollection
+
+
+{/* <ServiceCard
+title={services[0].title}
+subtitle={services[0].subtitle}
+/>
+<ServiceCard
+title={services[1].title}
+subtitle={services[0].subtitle}
+/>
+<ServiceCard
+title={services[2].title}
+subtitle={services[0].subtitle}
+/>
+<ServiceCard
+title={services[3].title}
+subtitle={services[0].subtitle}
+/> */}
